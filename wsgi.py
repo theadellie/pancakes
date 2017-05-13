5c2eed2c2b79cdf3250bbbcb8eda3f1fcdb11e75
Addler <theadellie@gmail.com>
2017-05-12 18:05:05 -0700

    dbtest

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,27 @@
+import os
+import psycopg2
+
+def dbconn():
+    return psycopg2.connect(
+        host =      os.getenv("POSTGRESQL_HOST", 'localhost'), 
+        user =      os.getenv("POSTGRESQL_USER", 'admin'),
+        password =  os.getenv("POSTGRESQL_PASSWORD", ''),
+        dbname =    os.getenv("POSTGRESQL_DATABASE", 'sampledb')
+    )
+
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! :)"
+    conn = dbconn()
+    print(conn)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM systems_populated")
+    rows = cur.fetchall()
+    print(rows)
+    conn.close
+    return "Hello World! :-) First value: {}, Second Value: {}".format(rows[0][0], rows[1][0])
 
 if __name__ == "__main__":
     application.run()
